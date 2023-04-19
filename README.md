eaa6601ec6f27c34b968b73bd997a2c3319a6a4d
brian <lbc.brian.chilin@gmail.com>
2023-04-19 03:07:48 -0700

    ready to start developing in src/main.rs
    imported RNG
    updated readme
    play around in main.rs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rust_heap_sort
- cecs343
+Cecs343
+I primary change files in 3 places:
+1) README.md		# this file
+2) Cargo.toml		# dependencies
+3) src/				# all source code
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let a: u32 = rng.gen();
+    let b: u32 = rng.gen();
+
+    println!("a: {}\nb: {}", a, b);
 }
