e182918421918b0b1bfea1f3138779cb8b51fcad
brian <lbc.brian.chilin@gmail.com>
2023-04-19 17:57:27 -0700

    almost finished generating original numbers
    step 1 on instructions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use rand::Rng;
+use std::collections::HashSet;
+
+// https://doc.rust-lang.org/rust-by-example/std/hash/hashset.html
 
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let a: u32 = rng.gen();
-    let b: u32 = rng.gen();
+    let mut heap: [u8; 32] = [0; 32];
+    let mut used_nums = HashSet::new();
 
-    println!("a: {}\nb: {}", a, b);
+    for i in 0..32 {
+        let n = rng.gen_range(10..=99);
+
+        // while loop here to keep generating n if it already exists
+        used_nums.insert(n);
+        print!("{} ", (used_nums.contains(&n)));
+
+        heap[i] = n;
+        print!("{} ", heap[i]);
+    }
 }
