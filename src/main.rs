7ea0aacf9bc4d2b0f411c61c7ce88ad3a8b9d7e0
brian <lbc.brian.chilin@gmail.com>
2023-04-25 02:21:19 -0700

    finish 00

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,24 @@ fn main() {
         array[i] = n;
     }
 
+    print!("New tree: ");
     display_tree(&array);
     //print!("{:?}", array);
 
     print!("Enter to continue");
     std::io::stdout().flush().unwrap();
-    //let mut input = String::new();
-    //std::io::stdin().read_line(&mut input).unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
     //print!("{}", input);
 
     heapsort(&mut array);
+
+    print!("Finished sorting: ");
+    display_tree(&array);
+    print!("{:?}", array);
 }
 
-fn display_tree(heap: &[u8; 31]) {
+fn display_tree(array: &[u8; 31]) {
     println!("
                                             {}
    	                                         |
@@ -43,43 +48,63 @@ fn display_tree(heap: &[u8; 31]) {
     {}----^----{}		   {}----^----{}           {}----^----{}           {}----^----{}
     |           |          |           |           |           |           |           |
 {}--^--{}   {}--^--{}  {}--^--{}   {}--^--{}   {}--^--{}   {}--^--{}   {}--^--{}   {}--^--{}",
-    heap[0], heap[1], heap[2], heap[3], heap[4], heap[5], heap[6], heap[7], heap[8], heap[9], heap[10], heap[11], heap[12], heap[13], heap[14], heap[15],
-             heap[16], heap[17], heap[18], heap[19], heap[20], heap[21], heap[22], heap[23], heap[24], heap[25], heap[26], heap[27], heap[28], heap[29], heap[30]);
+    array[0], array[1], array[2], array[3], array[4], array[5], array[6], array[7], array[8], array[9], array[10], array[11], array[12], array[13], array[14], array[15],
+             array[16], array[17], array[18], array[19], array[20], array[21], array[22], array[23], array[24], array[25], array[26], array[27], array[28], array[29], array[30]);
 }
 
 fn heapsort(array: &mut [u8; 31]) {
-    build_min_heap(array);
+    build_max_heap(array);
+    print!("\nAs a max-heap: ");
     display_tree(array);
 
-    for i in 0..31 {
-        println!("{}", i);
+    for i in 0..30 {
+        let temp = array[0];
+        array[0] = array[30-i];
+        array[30-i] = temp;
+        println!("{:?}", array); // instruction 5a
+        max_heapify(array, 0, 30-i);
     }
+
+    //_sorted(array);
 }
 
-fn build_min_heap(array: &mut [u8]) {
+fn _sorted(array: &[u8]) {
+    let mut last = array[0];
+    for i in 1..array.len() {
+        if array[i] < last {
+            print!("unsorted, {} shouldn't come after {}", array[i], last);
+            return
+        }
+        last = array[i];
+    }
+    print!("sorted!");
+}
+
+fn build_max_heap(array: &mut [u8]) {
     //this among other heap functions i copy from
     //Introduction to Algorithms 3ed by Thomas H. Cormen
     //i studied and learned these well last semester in CECS328
     for i in (0..(array.len()/2)).rev() {
-        min_heapify(array, i);
+        max_heapify(array, i, array.len());
     }
 }
 
-fn min_heapify(array: &mut [u8], index: usize) {
-    let left_i  = (index * 2) + 2;
-    let right_i = (index * 2) + 1;
+fn max_heapify(array: &mut [u8], index: usize, heap_size: usize) {
+    // heap_size =< array.len() should be true
+    let left_i  = (index * 2) + 1;
+    let right_i = (index * 2) + 2;
 
-    let mut smallest = index;
-    if left_i < array.len() && array[left_i] < array[smallest] {
-        smallest = left_i;
+    let mut largest = index;
+    if left_i < heap_size && array[left_i] > array[largest]{
+        largest = left_i;
     }
-    if right_i < array.len() && array[right_i] < array[smallest] {
-        smallest = right_i
+    if right_i < heap_size && array[right_i] > array[largest] {
+        largest = right_i
     }
-    if smallest != index {
+    if largest != index {
         let temp = array[index];
-        array[index] = array[smallest];
-        array[smallest] = temp;
-        min_heapify(array, smallest)
+        array[index] = array[largest];
+        array[largest] = temp;
+        max_heapify(array, largest, heap_size);
     }
 }
