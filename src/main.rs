427ed8a32d06fe8da3d439d087f2088ca9cbf887
brian <lbc.brian.chilin@gmail.com>
2023-04-25 11:34:23 -0700

    heading
    required to be added by syllabus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Brian C Stevenson
+// CECS 342-07
+// Prog 5 - Rusty Heapsort
+// May 4, 2023
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program
+
 use rand::Rng;
 use std::collections::HashSet;
 use std::io::{Write};
